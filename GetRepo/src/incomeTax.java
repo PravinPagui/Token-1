c8e3b4a668e6c6ff6a7966a406d534c1aac7b5da
admin <admin@Bcalab2-m23>
2022-04-28 09:06:38 +0530

    Commit 1

---
+++ GetRepo/src/incomeTax.java
@@ -0,0 +1,18 @@
+
+public class incomeTax {
+
+    public class incomeTAX {
+	double basic_Salary;
+	incomeTax(double basic_salary)
+	{	
+		this.basic_salay = basic_salary;
+	}
+	
+    }
+    
+}
+	
+  
+
+    
+    
